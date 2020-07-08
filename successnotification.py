f3fdc6c909863bd1b87644c02ee97e795a687477
Minshu123 <minshunitrkl@gmail.com>
2020-07-08 08:50:46 +0530

    second commit

---
+++ successnotification.py
@@ -0,0 +1,11 @@
+import smtplib
+sender_email="minshudwarika@gmail.com"
+rec_email="minshunitrkl@gmail.com"
+password="password"
+message="Hey ,Your website has been deployed and site is working fine."
+server=smtplib.SMTP("smtp.gmail.com",587) 
+server.starttls()
+server.login(sender_email,password)
+print("Login success")
+server.sendmail(sender_email,rec_email,message)
+print("Email  has been sent")
---
+++ unsuccessnotification.py
@@ -0,0 +1,11 @@
+
+import smtplib
+sender_email="minshudwarika@gmail.com"
+rec_email="minshunitrkl@gmail.com"
+password="password"
+message="hey ,Something wrong with the code or  the set up .please check out.  "
+server=smtplib.SMTP("smtp.gmail.com",587) 
+server.starttls()
+server.login(sender_email,password)
+print("Login success")
+server.sendmail(sender_email,rec_email,message)
