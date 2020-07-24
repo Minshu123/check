aff0f8467d623511150960c37fc6e20cc1a2c312
Minshu123 <47297558+Minshu123@users.noreply.github.com>
2020-07-24 16:37:23 +0530

    last commit

--- unsuccessnotification.py
+++ unsuccessnotification.py
@@ -2,7 +2,7 @@
 import smtplib
 sender_email="minshudwarika@gmail.com"
 rec_email="minshunitrkl@gmail.com"
-password="resonance"
+password="password"
 message="hey ,Something wrong with the code or  the set up .please check out.  "
 server=smtplib.SMTP("smtp.gmail.com",587) 
 server.starttls()
