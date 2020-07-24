a198e25afdc9b83dfe77a9a0254971a15551982e
Minshu123 <47297558+Minshu123@users.noreply.github.com>
2020-07-24 16:36:45 +0530

    lasst commit

--- successnotification.py
+++ successnotification.py
@@ -1,7 +1,7 @@
 import smtplib
 sender_email="minshudwarika@gmail.com"
 rec_email="minshunitrkl@gmail.com"
-password="resonance"
+password="password"
 message="Hey ,Your website has been deployed and site is working fine."
 server=smtplib.SMTP("smtp.gmail.com",587) 
 server.starttls()
